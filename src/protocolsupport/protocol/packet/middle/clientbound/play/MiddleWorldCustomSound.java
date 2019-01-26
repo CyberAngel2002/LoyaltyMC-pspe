c20f7d1687087325ff37462a986263f440211bdf
WesleyVanNeck <wesleyvanneck105@outlook.com>
2019-01-26 15:37:44 +0000

    Revert "Sound fix"

--- src/protocolsupport/protocol/packet/middle/clientbound/play/MiddleWorldCustomSound.java
+++ src/protocolsupport/protocol/packet/middle/clientbound/play/MiddleWorldCustomSound.java
@@ -1,6 +1,7 @@
 package protocolsupport.protocol.packet.middle.clientbound.play;
 
 import io.netty.buffer.ByteBuf;
+import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.ConnectionImpl;
 import protocolsupport.protocol.packet.middle.ClientBoundMiddlePacket;
 import protocolsupport.protocol.serializer.StringSerializer;
