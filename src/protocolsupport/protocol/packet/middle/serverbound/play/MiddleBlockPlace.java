a60b6c0225e4e04f0d516ab2cad6489bd7a32c5a
Emilia1012 <wesleyvanneck105@outlook.com>
2019-04-13 14:24:04 +0200

    remove the more debug

--- src/protocolsupport/protocol/packet/middle/serverbound/play/MiddleBlockPlace.java
+++ src/protocolsupport/protocol/packet/middle/serverbound/play/MiddleBlockPlace.java
@@ -32,7 +32,7 @@ public abstract class MiddleBlockPlace extends ServerBoundMiddlePacket {
 
 	public static ServerBoundPacketData create(Position position, int face, UsedHand hand, float cX, float cY, float cZ) {
 		if (face != -1) {
-			System.out.println("USE ITEM");
+			//System.out.println("USE ITEM");
 			ServerBoundPacketData creator = ServerBoundPacketData.create(ServerBoundPacket.PLAY_USE_ITEM);
 			PositionSerializer.writePosition(creator, position);
 			VarNumberSerializer.writeVarInt(creator, face);
@@ -42,7 +42,7 @@ public abstract class MiddleBlockPlace extends ServerBoundMiddlePacket {
 			creator.writeFloat(cZ);
 			return creator;
 		} else {
-			System.out.println("PLACE BLOCK");
+			//System.out.println("PLACE BLOCK");
 			ServerBoundPacketData creator = ServerBoundPacketData.create(ServerBoundPacket.PLAY_BLOCK_PLACE);
 			MiscSerializer.writeVarIntEnum(creator, hand);
 			return creator;
