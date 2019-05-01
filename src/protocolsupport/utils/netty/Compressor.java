9d9d8f8ce67c14ad7ba10a8bba815888a6955576
WesleyVanNeck <wesleyvanneck105@outlook.com>
2019-05-01 14:30:48 +0200

    Update Compressor.java

--- src/protocolsupport/utils/netty/Compressor.java
+++ src/protocolsupport/utils/netty/Compressor.java
@@ -13,7 +13,7 @@ import protocolsupportbuildprocessor.Preload;
 @Preload
 public class Compressor {
 
-	protected static final int compressionLevel = JavaSystemProperty.getValue("compressionlevel", 3, Integer::parseInt);
+	protected static final int compressionLevel = JavaSystemProperty.getValue("compressionlevel", 10, Integer::parseInt);
 
 	static {
 		ProtocolSupport.logInfo("Compression level: " + compressionLevel);
