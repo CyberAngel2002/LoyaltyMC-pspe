bede28cb8e7505f1cbcf9f187c0a3f3d87af481f
WesleyVanNeck <wesleyvanneck105@outlook.com>
2019-05-09 09:08:57 +0200

    revert this

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/StartGame.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/StartGame.java
@@ -133,9 +133,9 @@ public class StartGame extends MiddleStartGame {
 		VarNumberSerializer.writeSVarInt(chunkradius, (int) Math.ceil((Bukkit.getViewDistance() + 1) * Math.sqrt(2)));
 		packets.add(chunkradius);
 
-		/*ClientBoundPacketData enableCommandsPacket = ClientBoundPacketData.create(PEPacketIDs.SET_COMMANDS_ENABLED);
+		ClientBoundPacketData enableCommandsPacket = ClientBoundPacketData.create(PEPacketIDs.SET_COMMANDS_ENABLED);
 		enableCommandsPacket.writeBoolean(true);
-		packets.add(enableCommandsPacket);*/
+		packets.add(enableCommandsPacket);
 
 		PECreativeInventory peInv = PECreativeInventory.getInstance();
 		ClientBoundPacketData creativeInventoryPacket = ClientBoundPacketData.create(PEPacketIDs.INVENTORY_CONTENT);
