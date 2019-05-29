7fc1a3c79bbcecdf0a9cda8da1244f03b1f2dfab
WesleyVanNeck <wesleyvanneck105@outlook.com>
2019-05-29 08:45:43 +0200

    fix

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/CustomPayload.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/CustomPayload.java
@@ -48,8 +48,7 @@ public class CustomPayload extends MiddleCustomPayload {
 	public static ClientBoundPacketData create(ProtocolVersion version, String tag, ByteBuf data) {
 		ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.CUSTOM_EVENT);
 		StringSerializer.writeString(serializer, version, tag);
-		VarNumberSerializer.writeVarInt(serializer, data.readableBytes());
-		serializer.writeBytes(data);
+		ArraySerializer.writeVarIntByteArray(serializer, data);
 		return serializer;
 	}
 
